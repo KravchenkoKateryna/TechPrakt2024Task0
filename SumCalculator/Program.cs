6f6c3a4c3417a9884559f0c5d50438a1d07f5341
KaterynaKravchenko <ipz222_kkm@student.ztu.edu.ua>
2024-01-27 19:34:00 +0200

    added sumcalculator

---
+++ SumCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть перше число:");
+        if (double.TryParse(Console.ReadLine(), out double num1))
+        {
+            Console.WriteLine("Введіть друге число:");
+            if (double.TryParse(Console.ReadLine(), out double num2))
+            {
+                double sum = num1 + num2;
+                Console.WriteLine($"Сума двох чисел: {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Некоректне друге число.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некоректне перше число.");
+        }
+    }
+}
